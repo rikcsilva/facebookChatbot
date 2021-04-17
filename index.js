ac5d80146471d4fc83a0daccee00f238200109fa
Ricardo Cavalcanti <rikcsilva@gmail.com>
2021-04-17 17:46:56 -0300

    Enabling functionality

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.get('/', function(req, res) {
 	res.send("Hi I am a chatbot")
 })
 
+token = ""
+
 // Facebook
 
 app.get('/webhook/', function(req, res) {
@@ -26,6 +28,38 @@ app.get('/webhook/', function(req, res) {
 	}
 	res.send("Wrong token")
 })
+
+ap.post('/webhook', function(req, res) {
+	let messaging_events = req.body.entry[0].messaging_events
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = messaging_events[i]
+		let sender = event.sender.id
+		if (event.message && event.message.text) {
+			let text = event.message.text
+			sendText(sender, "Text echo: " + text.substring(0,100))
+		}
+	}
+	res.sendStatus(200)
+})
+
+function sendText(sender, text) {
+	let messageData = {text: text}
+	request({
+		url: "https://graph.facebook.com/v10.0/me/messages",
+		qs : {access_token, token},
+		method: "POST",
+		json: {
+			receipt: {id: sender},
+			message : messageData,
+		}
+	}, function(error, response, body) {
+			if (error) {
+				console.log("sending error")			 
+			} else if (response.body.error) {
+				console.log("response body error")
+			}
+	})
+}
 
 app.listen(app.get('port'), function() {
 	console.log("runing: port")
